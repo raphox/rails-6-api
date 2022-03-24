5146f153d7f67f9d86f3e5f768395b4b622d3825
Raphael Araújo <raphox.araujo@gmail.com>
2022-03-24 10:10:22 -0300

    Apply suggestions from code review

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -15,6 +15,16 @@ module Api
       def show
         render json: @post
       end
+      # POST /api/v1/posts
+      def create
+        @post = Post.new(post_params)
+
+        if @post.save
+          render json: @post, status: :created, location: api_post_url(@post)
+        else
+          render json: @post.errors, status: :unprocessable_entity
+        end
+      end
 
       # PATCH/PUT /api/v1/posts/1
       def update
@@ -23,6 +33,10 @@ module Api
         else
           render json: @post.errors, status: :unprocessable_entity
         end
+      end
+      # DELETE /api/v1/posts/1
+      def destroy
+        @post.destroy
       end
 
       private
